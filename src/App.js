cdc65df7fdb7e65583163b512aa3e9532ac8b039
DannielChoi <danniel.choi@gmail.com>
2020-12-18 16:41:29 +0900

    connect firebase-firestore

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
 import React, {Component} from 'react';
 import TaskAdd from './TaskAdd';
 import TaskDisplay from './TaskDisplay';
-
+import { firestore } from "./firebase";
 class App extends Component {
   state = {
     tasks:[
-      {todo: "할 일 1"},
-      {todo: "할 일 2"},
-      {todo: "할 일 3"},
-      {todo: "할 일 4"},
     ],
     task:''
   }
+  componentDidMount(){
+    const tasks = [...this.state.tasks]
+    firestore.collection('tasks').get()
+      .then(docs=>{
+        docs.forEach(doc => {
+          tasks.push({ todo: doc.data().todo, id: doc.id })
+        })
+        this.setState({tasks})
+      })
+  }
   onClickHandler = (e) => {
     e.preventDefault();
-    const task = { todo: this.state.task };
-    const tasks = [...this.state.tasks, task]
-    this.setState({
-      tasks,
-      task: ''
-    })
+    firestore.collection('tasks').add({todo:this.state.task})
+      .then(r=>{
+        const tasks = [...this.state.tasks, {todo:this.state.task, id:r.id}];
+        this.setState({
+          tasks,
+          task:''
+        })
+      })
   }
   onChangeHandler = (e) => {
     this.setState({
       task:e.target.value
     })
   }
-  deleteHandler = (idx) => {
-    const tasks = this.state.tasks.filter((task,i)=>i!==idx)
-    this.setState({tasks});
+  deleteHandler = (id) => {
+    firestore.collection('tasks').doc(id).delete()
+      .then(()=>{
+        const tasks = this.state.tasks.filter((task) => task.id !== id)
+        this.setState({tasks});
+      })
+    // const tasks = this.state.tasks.filter((task,i)=>i!==idx)
+    // this.setState({tasks});
   }
   render() {
-    const taskDisplay = this.state.tasks.map((task,i)=>{
+    const taskDisplay = this.state.tasks.map((task)=>{
       return (
-        <div key={i}>
+        <div key={task.id}>
           <p>{task.todo}</p>
-          <button onClick={()=>this.deleteHandler(i)}>삭제</button>
+          <button onClick={()=>this.deleteHandler(task.id)}>삭제</button>
         </div>
       )
     })
--- src/TaskDisplay.js
+++ src/TaskDisplay.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const TaskDisplay = ({tasks, deleteHandler}) => {
-    const taskDisplay = tasks.map((task,i) => { 
+const TaskDisplay = ({ tasks, deleteHandler }) => {
+    const taskDisplay = tasks.map((task) => { 
       return (
-        <div key={i} class="box">
+        <div key={task.id} class="box">
           <nav class="level">
             <div class="level-left">
               <p class="title">{task.todo}</p>
             </div>
             <div class="level-right">
-              <button class="button is-danger" onClick={() => deleteHandler(i)}>삭제</button>
+              <button class="button is-danger" onClick={() => deleteHandler(task.id)}>삭제</button>
             </div>
           </nav>
         </div>
